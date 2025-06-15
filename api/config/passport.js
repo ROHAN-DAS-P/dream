fcef3568a10e7f2a6905da771ef355b31857a60e
Rohan Das P <rohan2003710@gmail.com>
2025-06-15 22:16:30 +0530

    changed the callback url

--- api/config/passport.js
+++ api/config/passport.js
@@ -35,7 +35,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/github/callback",
+      callbackURL: `${process.env.FRONTEND_URL}/api/auth/github/callback`,
       scope: ['user:email'], // ✅ Request email scope
     },
     async (accessToken, refreshToken, profile, done) => {
